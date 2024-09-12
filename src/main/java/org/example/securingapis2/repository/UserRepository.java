ab36f2073fb458ef957803590764f86cec0441e1
chloe514 <chloevans22@gmail.com>
2024-09-11 20:18:27 -0400

    Update UserRepository.java

--- src/main/java/org/example/securingapis2/repository/UserRepository.java
+++ src/main/java/org/example/securingapis2/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Define any custom query methods if needed
+
 }
 
